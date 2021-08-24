f33ad5bff600490586e3680a8fc2331db30bc9b1
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-08-24 15:21:14 -0700

    Work

--- Bellwork JS Review using MAP.js
+++ Bellwork JS Review using MAP.js
@@ -1,40 +1,35 @@
 // Write a function capitalize that takes a string and uses .map to return the same string in all caps.
+var capital = [];
+
 var capitalize = function(string) {
-    var capital = [];
 
     string.split(" ").map(word => {
         var capitalLetter = word.toUpperCase();
         capital.push(capitalLetter);
     })
     console.log(capital.join(" "));
-    console.log(capital);
 };
 
-capitalize('hallo i am barik.');
-
-2.
 // Now write a new function called swapCase that takes a string of words and uses .map and your newly written capitalize() function to return a string where every other word is in all caps.
-// Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
-// ex: swapCase('hey girl, lets javascript together sometime') // => "HEY gurl, LETS javascript TOGETHER sometime"
 var swapCase = function(string){
     capitalize(string);
 
-    console.log(capital);
+    var swapCaps = capital;
     for (i = 1; i < capital.length; i+=2) {
-        capital[i] = capital[i].toLowerCase();
+        swapCaps[i] = capital[i].toLowerCase();
     }
-    console.log(capital);
+    console.log(swapCaps.join(" "));
 }
 
-swapCase('this is the second question')
+swapCase('this is the second question');
 
-
-3.
-// Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
-// alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.CharCodeAt() and see if you can use
-// Ascii code to acomplish this.
+// Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.CharCodeAt() and see if you can use Ascii code to accomplish this.
 // ex. shiftLetters('hello') // => 'ifmmp'
 // ex. (shiftLetters('abcxyz') // => "bcdyz{"
-var shiftLetters = function(string){
+var shiftLetters = function(string) {
+    string.split('').map((letter) => {
+        letter
+    });
+
 
 }
